e4be94ffbe44f861c85e2862151b296280e0bad2
Justin Liew <jliew@fastly.com>
2021-06-24 16:21:35 -0700

    lower particle intensity and size to improve perf.

--- src/entities/particle.rs
+++ src/entities/particle.rs
@@ -52,7 +52,7 @@ impl Particle {
 /// Generates a new explosion of the given intensity at the given position.
 /// This works best with values between 5 and 25
 pub fn make_explosion(particles: &mut Vec<Particle>, position: &Point, intensity: u8, colour_index: ColourIndex) {
-    for rotation in itertools_num::linspace(0.0, 2.0 * ::std::f64::consts::PI, 30) {
+    for rotation in itertools_num::linspace(0.0, 2.0 * ::std::f64::consts::PI, 15) {
         for ttl in (1..intensity).map(|x| (x as f64) / 10.0) {
             particles.push(Particle::new(Vector::new(position.clone(), rotation), ttl, colour_index));
         }
