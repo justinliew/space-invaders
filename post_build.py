703f729a6b2d46e6e64aaaa26a1091f1c1442d0d
Justin Liew <jliew@fastly.com>
2021-06-24 15:50:59 -0700

    administrative changes: - don't run wasm-gc; it doesn't run properly on github pages - update readme

--- post_build.py
+++ post_build.py
@@ -4,4 +4,4 @@ from shutil import copyfile
 from subprocess import call
 
 copyfile('target/wasm32-unknown-unknown/release/space_invaders.wasm', 'html/space_invaders.wasm')
-call(['wasm-gc', 'html/space_invaders.wasm', 'html/program.wasm'])
+#call(['wasm-gc', 'html/space_invaders.wasm', 'html/program.wasm'])
